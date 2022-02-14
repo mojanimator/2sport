7cf83274a4f50077a6644db6bf60dfce8c438e3b
mmr <moj2raj2@gmail.com>
2022-02-14 07:24:35 +0330

    add pay to register

--- app/Http/Helpers/Telegram.php
+++ app/Http/Helpers/Telegram.php
@@ -84,13 +84,13 @@ class Telegram
         $url = "https://api.telegram.org/bot" . env('TELEGRAM_BOT_TOKEN', 'YOUR-BOT-TOKEN') . "/" . $method;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
 
         $res = curl_exec($ch);
+        curl_close($ch);
 //        echo $res;
         $res = json_decode($res);
-        curl_close($ch);
 //        if ($res && $res->ok == false)
 //            self::sendMessage(Helper::$logs[0], /*"[" . $datas['chat_id'] . "](tg://user?id=" . $datas['chat_id'] . ") \n" .*/
 //                json_encode($datas) . "\n" . $res->description, null, null, null);
